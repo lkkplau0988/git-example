de23d86b3f6c6d73b9c1ceb9e7081c9772e787db
lkkplau0988 <54789733+lkkplau0988@users.noreply.github.com>
2020-08-22 22:42:08 +0800

    Update hello.py

--- hello.py
+++ hello.py
@@ -5,5 +5,6 @@ print('add message in github for testing')
 print('final test for git')
 print('some test for github')
 print('test from peter')
+print('test from peter in github for pull')
 #push test on 21-8-20
 #push test on 22-8-20
