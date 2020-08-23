0ab41f3785ea004bb42c1b5a4db990cc82795811
lkkplau0988 <54789733+lkkplau0988@users.noreply.github.com>
2020-08-23 14:56:01 +0800

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,6 +1,5 @@
 print("I want to check different filefolder push on 23/8")
 print("use:git push -u <remote name><branch name> can push any file to github")
-#check pull test
-#check push test
+
 #check the merge method
 #check the merge method,can't compare
